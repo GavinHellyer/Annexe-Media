83acf68fcc7750beb43c75cb17624be0d3876aea
Richard Hodsdon <hodsdon.richard@gmail.com>
2011-09-04 14:01:38 +0200

    Added .htaccess for neater app urls Fixed core.class commenting, to hopefully work on all systems. Testing required. Added new dashboard app, which will become the default to load All other Apps

---
+++ apps/dashboard/admin.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_REQUEST['key']) && $_REQUEST['key'] == 'something') {
+  
+} else {
+  echo "You do not have access to view this page.";
+}
+?>
---
+++ module/dashboard/loader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @ver 1.0
+ * @module Dashboard
+ * @description GVA Apps Dashbaord
+ * @css true
+ * @js true
+*/
+
+return 'Welcome';
+?>
